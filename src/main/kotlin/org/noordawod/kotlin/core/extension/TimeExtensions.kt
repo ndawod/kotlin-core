a44b6b84daf864b393437b743904c0c9db81fb9f
Noor Dawod <hi@noordawod.me>
2020-03-29 20:14:33 +0200

    Added a couple of constants for time calculations.

--- src/main/kotlin/org/noordawod/kotlin/core/extension/TimeExtensions.kt
+++ src/main/kotlin/org/noordawod/kotlin/core/extension/TimeExtensions.kt
@@ -30,19 +30,29 @@ import kotlin.math.absoluteValue
 /**
  * How many milliseconds in one second.
  */
-const val MILLIS_IN_1_SEC: Long = 1_000L
+const val MILLIS_IN_1_SECOND: Long = 1_000L
+
+/**
+ * How many seconds in one hour.
+ */
+const val SECONDS_IN_1_HOUR: Long = 3_600L
+
+/**
+ * How many seconds in one day.
+ */
+const val SECONDS_IN_1_DAY: Long = 86_400L
 
 /**
  * Converts an optional [Int] into a [java.util.Date] on success, null otherwise.
  */
 fun Int?.toDate(): java.util.Date? =
-  if (null == this) null else java.util.Date(this * MILLIS_IN_1_SEC)
+  if (null == this) null else java.util.Date(this * MILLIS_IN_1_SECOND)
 
 /**
  * Converts an optional [Int] into a [java.util.Date] on success, current time otherwise.
  */
 fun Int?.toDateOr(fallback: java.util.Date = java.util.Date()): java.util.Date =
-  if (null == this) fallback else java.util.Date(this * MILLIS_IN_1_SEC)
+  if (null == this) fallback else java.util.Date(this * MILLIS_IN_1_SECOND)
 
 /**
  * Converts an optional [Long] into a [java.util.Date] on success, null otherwise.
@@ -61,14 +71,14 @@ fun Long?.toDateOr(fallback: java.util.Date = java.util.Date()): java.util.Date 
  * since UNIX epoch on success, null otherwise.
  */
 fun java.util.Date?.epoch(): Int? =
-  if (null == this) null else (this.time / MILLIS_IN_1_SEC).toInt()
+  if (null == this) null else (this.time / MILLIS_IN_1_SECOND).toInt()
 
 /**
  * Converts an optional [java.util.Date] into an [Int] representing the seconds that passed
  * since UNIX epoch on success, current time otherwise.
  */
 fun java.util.Date?.epochOr(fallback: java.util.Date = java.util.Date()): Int =
-  ((this ?: fallback).time / MILLIS_IN_1_SEC).toInt()
+  ((this ?: fallback).time / MILLIS_IN_1_SECOND).toInt()
 
 /**
  * Adds the amount of [millis] to this [java.util.Date] instance, and returns it.
@@ -85,7 +95,7 @@ fun java.util.Date.plusMillis(millis: Int): java.util.Date = plusMillis(millis.t
  * Adds the amount of [seconds] to this [java.util.Date] instance, and returns it.
  */
 fun java.util.Date.plusSeconds(seconds: Long): java.util.Date =
-  plusMillis(seconds * MILLIS_IN_1_SEC)
+  plusMillis(seconds * MILLIS_IN_1_SECOND)
 
 /**
  * Adds the amount of [seconds] to this [java.util.Date] instance, and returns it.
@@ -107,7 +117,7 @@ fun java.util.Date.minusMillis(millis: Int): java.util.Date = minusMillis(millis
  * Subtracts the amount of [seconds] from this [java.util.Date] instance, and returns it.
  */
 fun java.util.Date.minusSeconds(seconds: Long): java.util.Date =
-  minusMillis(seconds * MILLIS_IN_1_SEC)
+  minusMillis(seconds * MILLIS_IN_1_SECOND)
 
 /**
  * Subtracts the amount of [seconds] from this [java.util.Date] instance, and returns it.
